10dcbf853a51fc7476854e4fe435adb2dabad887
Carlos R. Arias <arias@spu.edu>
2020-04-21 17:14:37 -0700

    Virtual ToString, good output!!

--- main.cpp
+++ main.cpp
@@ -16,17 +16,21 @@ using std::vector;
 int main() {
 	srand(time(nullptr)); // This line is VERY needed for warrior to work well
 
-	vector<Person> list;
-	list.push_back(Person());
-	list.push_back(Person("John"));
-	list.push_back(Person("Peter", 2000, 01, 01));
-	list.push_back(Employee("Manny", Date(1995, 02, 22), 3000));
+	vector<Person*> list;
+	list.push_back(new Person());
+	list.push_back(new Person("John"));
+	list.push_back(new Person("Peter", 2000, 01, 01));
+	list.push_back(new Employee("Manny", Date(1995, 02, 22), 3000));
 
-	for (Person p : list)
-		cout << p.ToString() << endl;
+	for (Person* p : list)
+		cout << p->ToString() << endl;
 
 	for (size_t i = 0; i < list.size(); i++)
-		cout << list[i].ToString() << endl;
+		cout << list[i]->ToString() << endl;
+
+	for (Person* p : list)
+		delete p;
+
 
 	return 0;
 }
